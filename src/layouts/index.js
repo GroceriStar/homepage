d898a3a03ea623cbdfd40fe8c3c3e5878b51b588
Arthur Tkachenko <atherdon@gmail.com>
2018-04-29 02:17:52 +0300

    xx

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,11 +1,14 @@
 import React  from 'react'
 import Helmet from 'react-helmet'
 import { Link, withPrefix } from 'gatsby-link'
+
 import '../assets/scss/main.scss'
+
 import Header  from '../components/Header'
 import Menu    from '../components/Menu'
 import Contact from '../components/Contact'
 import Footer  from '../components/Footer'
+
 
 class Template extends React.Component {
 
@@ -41,8 +44,26 @@ class Template extends React.Component {
 
         return (
             <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
-                <Helmet>
-                    <link rel="stylesheet" href={withPrefix('skel.css')} />
+
+                <Helmet
+                  title={data.site.siteMetadata.title}
+                  meta={[
+                    { charset: 'utf-8' },
+                    { name: 'description', content: 'GS' },
+                    { name: 'keywords', content: 'grocery list, shopping list template' },
+                    { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+                    { name: 'theme-color', content: '#000000' }
+                  ]}
+                >
+                  <link rel="stylesheet" href={withPrefix('skel.css')} />
+                  {/*}<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+                  <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
+
+
+
+                  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>*/}
                 </Helmet>
                 <div id="wrapper">
                     <Header onToggleMenu={this.handleToggleMenu} />
