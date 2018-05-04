ff77432c488dec86ee9194b22f3d4e6098e015f3
Arthur Tkachenko <atherdon@gmail.com>
2018-05-04 03:46:50 +0300

    xxx

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,69 +2,78 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 
-const Footer = (props) => (
-    <footer id="footer">
-        <div className="inner">
-            <ul className="icons">
-              <li>
-                <Link className="icon alt fa-facebook" to="/auth/facebook">
-                  <span className="label">
-                  Continue with Facebook</span>
-                </Link>
-              </li>
-              <li>
-                <Link className="icon alt fa-github" to="https://github.com/GroceriStar">
-                  <span className="label">
-                  GitHub Project</span>
-                </Link>
-              </li>
-              <li>
-                <Link className="icon alt fa-linkedin" to="https://www.linkedin.com/company/11486092/">
-                  <span className="label">
-                  LinkedIn</span>
-                </Link>
-              </li>
-              <li>
-                <Link className="icon alt fa-envelope" to="mailto:arthur.tkachenko.netweight@gmail.com">
-                  <span className="label">
-                  Email Us</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/terms">
-                  <span className="label">
-                  Terms</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/privacy">
-                  <span className="label">
-                  Privacy</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/credits">
-                    <span className="label">
-                    Credits</span>
-                </Link>
-              </li>
-            </ul>
+const Footer = ( props ) => (
 
-            <ul className="copyright">
-                <li>
-                  &copy; Groceristar
-                </li>
-                <li>
-                  Design:
-                  <Link  to="https://html5up.net">
-                  HTML5 UP
-                  </Link>
+  <footer id="footer">
+      <div className="inner">
+          <ul className="icons">
+            <li>
+              <Link className="icon alt fa-facebook" to="/auth/facebook">
+                <span className="label">
+                Continue with Facebook</span>
+              </Link>
+            </li>
+            <li>
+              <Link className="icon alt fa-github" to="https://github.com/GroceriStar">
+                <span className="label">
+                GitHub Project</span>
+              </Link>
+            </li>
+            <li>
+              <Link className="icon alt fa-linkedin" to="https://www.linkedin.com/company/11486092/">
+                <span className="label">
+                LinkedIn</span>
+              </Link>
+            </li>
+            <li>
+              <Link className="icon alt fa-twitter" to="https://twitter.com/GroceriStar">
+                <span className="label">
+                Twitter
+                </span>
+              </Link>
+            </li>
+            <li>
+              <Link className="icon alt fa-envelope" to="mailto:arthur.tkachenko.netweight@gmail.com">
+                <span className="label">
+                Email Us</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/terms">
+                <span className="label">
+                Terms</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/privacy">
+                <span className="label">
+                Privacy</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/credits">
+                  <span className="label">
+                  Credits
+                  </span>
+              </Link>
+            </li>
+          </ul>
 
-                </li>
-            </ul>
+          <ul className="copyright">
+              <li>
+                &copy; Groceristar
+              </li>
+              <li>
+                Design:
+                <Link  to="https://html5up.net">
+                HTML5 UP
+                </Link>
 
-        </div>
-    </footer>
+              </li>
+          </ul>
+
+      </div>
+  </footer>
 )
 
 export default Footer
