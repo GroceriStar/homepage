765599dc9c809bb5876735694c64bb84d654c663
Arthur Tkachenko <atherdon@gmail.com>
2018-05-11 13:30:27 +0300

    xx

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,30 +18,27 @@ import Bottom      from '../components/Bottom'
 
 import data  from '../assets/data/first'
 
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
-
-
-import picz01 from '../assets/images/food-1.jpg'
-import picz02 from '../assets/images/food-2.jpg'
-import picz03 from '../assets/images/food-3.jpg'
-import picz04 from '../assets/images/food-4.jpg'
-import picz05 from '../assets/images/food-5.jpg'
-import picz06 from '../assets/images/food-6.jpg'
-import picz07 from '../assets/images/food-7.jpg'
-import picz08 from '../assets/images/food-8.jpg'
-// import picz09 from '../assets/images/food-9.jpg'
-import picz010 from '../assets/images/food-10.jpg'
-import picz011 from '../assets/images/food-11.jpg'
-import picz012 from '../assets/images/food-12.jpg'
-import picz013 from '../assets/images/food-13.jpg'
-// import pic06 from '../assets/images/food-14.jpg'
-// import pic06 from '../assets/images/food-15.jpg'
-// import pic06 from '../assets/images/food-16.jpg'
+// import pic01 from '../assets/images/pic01.jpg'
+// import pic02 from '../assets/images/pic02.jpg'
+// import pic03 from '../assets/images/pic03.jpg'
+// import pic04 from '../assets/images/pic04.jpg'
+// import pic05 from '../assets/images/pic05.jpg'
+// import pic06 from '../assets/images/pic06.jpg'
+//
+//
+// import picz01 from '../assets/images/food-1.jpg'
+// import picz02 from '../assets/images/food-2.jpg'
+// import picz03 from '../assets/images/food-3.jpg'
+// import picz04 from '../assets/images/food-4.jpg'
+// import picz05 from '../assets/images/food-5.jpg'
+// import picz06 from '../assets/images/food-6.jpg'
+// import picz07 from '../assets/images/food-7.jpg'
+// import picz08 from '../assets/images/food-8.jpg'
+//
+// import picz010 from '../assets/images/food-10.jpg'
+// import picz011 from '../assets/images/food-11.jpg'
+// import picz012 from '../assets/images/food-12.jpg'
+// import picz013 from '../assets/images/food-13.jpg'
 
 
 
